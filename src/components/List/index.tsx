a86e2bf864cf0fd373feb5715efe0f1ba3d4970e
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-05-20 19:37:10 -0300

    Component: List

---
+++ src/components/List/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <aside>
+      <h2>Daily Studies</h2>
+      <ul>
+        <li>
+            <h3>
+                React
+            </h3>
+            <span>
+                02:00:00
+            </span>
+        </li>
+        <li>
+            <h3>TypeScript</h3>
+            <span>01:00:00</span>
+        </li>
+      </ul>
+    </aside>
+  );
+};
+
+export default List;
