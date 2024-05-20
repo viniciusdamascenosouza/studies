8aca8c68493fc6b7c159970da339a6093bc1c533
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-05-20 19:49:56 -0300

    Rendering items from an array

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 
 const List = () => {
+  const tarefas = [
+    {
+      task: "React",
+      time: "02:00:00",
+    },
+    {
+      task: "TypeScript",
+      time: "01:00:00",
+    },
+    {
+        task: "Styled-components",
+        time: "01:00:00",
+    },
+  ];
+
   return (
     <aside>
       <h2>Daily Studies</h2>
       <ul>
-        <li>
-            <h3>
-                React
-            </h3>
-            <span>
-                02:00:00
-            </span>
-        </li>
-        <li>
-            <h3>TypeScript</h3>
-            <span>01:00:00</span>
-        </li>
+        {tarefas.map(({task, time}, index) => (
+          <li key={index}>
+            <h3>{task}</h3>
+            <span>{time}</span>
+          </li>
+        ))}
       </ul>
     </aside>
   );
